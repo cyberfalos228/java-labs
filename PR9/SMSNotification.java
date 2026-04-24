3c9b2e79b066e13d7ebbf98ae92c952e2f15ea05
cyberfalos228 <dimkaura777@gmail.com>
2026-04-25 00:01:43 +0200

    Create SMSNotification.java

---
+++ PR9/SMSNotification.java
@@ -0,0 +1,13 @@
+public class SMSNotification extends Notification {
+    private String phoneNumber;
+
+    public SMSNotification(String message, String phoneNumber) {
+        super(message);
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public void send() {
+        System.out.println("[SMS] До: " + phoneNumber + " | Повідомлення: " + message);
+    }
+}
